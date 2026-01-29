fb1f599efa052f0d6f588bdb5672bb49742838ad
Mihai <mihai.hanga@gmail.com>
2026-01-29 15:53:14 +0200

    Updated src

---
+++ src/libraries/helpers/Errors.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.33;
+
+/**
+ * @title Errors library
+ * @author mhng
+ * @notice Defines different error messages emitted by the contracts of the protocol
+ */
+library Errors {
+    error PPP__InvalidAddress();
+    error PPP__InvalidAmount();
+}
--- src/libraries/logic/FundProcessorLogic.sol
+++ src/libraries/logic/FundProcessorLogic.sol
@@ -3,20 +3,23 @@
 pragma solidity 0.8.33;
 
 import {Processor_Storage} from "../../protocol/Processor_Storage.sol";
+import {Errors} from "../helpers/Errors.sol";
 
 /**
  *
  */
 library FundProcessorLogic {
-    error PPP__InvalidAmount();
-
-    function executeFundProcessor(uint256 amount) external {
+    function executeFundProcessor(
+        mapping(address => uint256) storage deposits,
+        uint256 amount
+    ) external {
         if (amount == 0) {
-            revert PPP__InvalidAmount();
+            revert Errors.PPP__InvalidAmount();
         }
-        //require(amount > 0, "Amount must be > 0");
+
         usdc.safeTransferFrom(msg.sender, address(this), amount);
         deposits[msg.sender] += amount;
+
         //emit Deposit(msg.sender, amount);
     }
 }
