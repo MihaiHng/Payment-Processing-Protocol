fba6712efbb0fbb62e68e5c5e79553c1e40a7a73
Mihai <mihai.hanga@gmail.com>
2026-02-10 13:26:14 +0200

    Updated src

---
+++ src/interfaces/IProcessorAddressesProvider.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.33;
+
+/**
+ * @title IProcessorAddressesProvider
+ * @author mhng
+ * @notice Defines the basic interface for a Processor Address Provider.
+ */
+interface IProcessorAddressesProvider {
+    /*//////////////////////////////////////////////////////////////
+                            EVENTS
+    //////////////////////////////////////////////////////////////*/
+    // event MarketIdSet(string indexed oldMarketId, string indexed newMarketId);
+
+    /**
+     * @dev Emitted when the Processor is updated.
+     * @param oldAddress The old address of the Processor
+     * @param newAddress The new address of the Processor
+     */
+    event ProcessorUpdated(
+        address indexed oldAddress,
+        address indexed newAddress
+    );
+
+    /**
+     * @dev Emitted when a new proxy is created.
+     * @param id The identifier of the proxy
+     * @param proxyAddress The address of the created proxy contract
+     * @param implementationAddress The address of the implementation contract
+     */
+    event ProxyCreated(
+        bytes32 indexed id,
+        address indexed proxyAddress,
+        address indexed implementationAddress
+    );
+
+    /**
+     * @dev Emitted when a new non-proxied contract address is registered.
+     * @param id The identifier of the contract
+     * @param oldAddress The address of the old contract
+     * @param newAddress The address of the new contract
+     */
+    event AddressSet(
+        bytes32 indexed id,
+        address indexed oldAddress,
+        address indexed newAddress
+    );
+
+    /**
+     * @dev Emitted when the implementation of the proxy registered with id is updated
+     * @param id The identifier of the contract
+     * @param proxyAddress The address of the proxy contract
+     * @param oldImplementationAddress The address of the old implementation contract
+     * @param newImplementationAddress The address of the new implementation contract
+     */
+    event AddressSetAsProxy(
+        bytes32 indexed id,
+        address indexed proxyAddress,
+        address oldImplementationAddress,
+        address indexed newImplementationAddress
+    );
+
+    /*//////////////////////////////////////////////////////////////
+                            FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    //function getMarketId() external view returns (string memory);
+
+    /**
+     * @notice Returns an address by its identifier.
+     * @dev The returned address might be an EOA or a contract, potentially proxied
+     * @dev It returns ZERO if there is no registered address with the given id
+     * @param id The id
+     * @return The address registered for the specified id
+     */
+    function getAddress(bytes32 id) external view returns (address);
+
+    /**
+     * @notice Returns the address of the Processor proxy.
+     * @return The Processor proxy address
+     */
+    function getProcessor() external view returns (address);
+
+    //function setMarketId(string memory newMarketId) external;
+
+    /**
+     * @notice Sets an address for an id replacing the address saved in the addresses map.
+     * @dev IMPORTANT Use this function carefully, as it will do a hard replacement
+     * @param id The id
+     * @param newAddress The address to set
+     */
+    function setAddress(bytes32 id, address newAddress) external;
+
+    /**
+     * @notice General function to update the implementation of a proxy registered with
+     * certain `id`. If there is no proxy registered, it will instantiate one and
+     * set as implementation the `newImplementationAddress`.
+     * @dev IMPORTANT Use this function carefully, only for ids that don't have an explicit
+     * setter function, in order to avoid unexpected consequences
+     * @param id The id
+     * @param newImplementationAddress The address of the new implementation
+     */
+    function setAddressAsProxy(
+        bytes32 id,
+        address newImplementationAddress
+    ) external;
+
+    /**
+     * @notice Updates the implementation of the Processor, or creates a proxy
+     * setting the new `Processor` implementation when the function is called for the first time.
+     * @param newProcessorImpl The new Processor implementation
+     */
+    function setProcessorImpl(address newProcessorImpl) external;
+}
--- src/protocol/configuration/ProcessorAddressProvider.sol
+++ src/protocol/configuration/ProcessorAddressProvider.sol
@@ -3,5 +3,140 @@
 pragma solidity 0.8.33;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {IProcessorAddressProvider} from "../../interfaces/IProcessorAddressProvider.sol";
+import {IProcessorAddressesProvider} from "../../interfaces/IProcessorAddressesProvider.sol";
 import {InitializableImmutableAdminUpgradeabilityProxy} from "../../misc/upgradeability/InitializableImmutableAdminUpgradeabilityProxy.sol";
+
+contract ProcessorAddressesProvider is Ownable, IProcessorAddressesProvider {
+    // Chain identifier(ex. Ethereum mainnet)
+    // string private _chainId;
+
+    // Map of registered addresses (identifier => registeredAddress)
+    mapping(bytes32 => address) private _addresses;
+
+    // Main identifiers
+    bytes32 private constant PROCESSOR = "PROCESSOR";
+
+    /**
+     * @dev Constructor.
+     * @param owner The owner address of this contract.
+     */
+    constructor(address owner) Ownable(owner) {}
+
+    /*//////////////////////////////////////////////////////////////
+                            VIEW FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @inheritdoc IProcessorAddressesProvider
+    function getAddress(bytes32 id) public view override returns (address) {
+        return _addresses[id];
+    }
+
+    /// @inheritdoc IProcessorAddressesProvider
+    function getProcessor() external view override returns (address) {
+        return getAddress(PROCESSOR);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            ADMIN FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @inheritdoc IProcessorAddressesProvider
+    function setAddress(
+        bytes32 id,
+        address newAddress
+    ) external override onlyOwner {
+        address oldAddress = _addresses[id];
+        _addresses[id] = newAddress;
+        emit AddressSet(id, oldAddress, newAddress);
+    }
+
+    /// @inheritdoc IProcessorAddressesProvider
+    function setAddressAsProxy(
+        bytes32 id,
+        address newImplementationAddress
+    ) external override onlyOwner {
+        address proxyAddress = _addresses[id];
+        address oldImplementationAddress = _getProxyImplementation(id);
+        _updateImpl(id, newImplementationAddress);
+        emit AddressSetAsProxy(
+            id,
+            proxyAddress,
+            oldImplementationAddress,
+            newImplementationAddress
+        );
+    }
+
+    /// @inheritdoc IProcessorAddressesProvider
+    function setProcessorImpl(
+        address newProcessorImpl
+    ) external override onlyOwner {
+        address oldProcessorImpl = _getProxyImplementation(PROCESSOR);
+        _updateImpl(PROCESSOR, newProcessorImpl);
+        emit ProcessorUpdated(oldProcessorImpl, newProcessorImpl);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    /**
+     * @notice Internal function to update the implementation of a specific proxied component of the protocol.
+     * @dev If there is no proxy registered with the given identifier, it creates the proxy setting `newAddress`
+     *   as implementation and calls the initialize() function on the proxy
+     * @dev If there is already a proxy registered, it just updates the implementation to `newAddress` and
+     *   calls the initialize() function via upgradeToAndCall() in the proxy
+     * @param id The id of the proxy to be updated
+     * @param newAddress The address of the new implementation
+     */
+    function _updateImpl(bytes32 id, address newAddress) internal {
+        address proxyAddress = _addresses[id];
+        InitializableImmutableAdminUpgradeabilityProxy proxy;
+        bytes memory params = abi.encodeWithSignature(
+            "initialize(address)",
+            address(this)
+        );
+
+        if (proxyAddress == address(0)) {
+            proxy = new InitializableImmutableAdminUpgradeabilityProxy(
+                address(this)
+            );
+            _addresses[id] = proxyAddress = address(proxy);
+            proxy.initialize(newAddress, params);
+            emit ProxyCreated(id, proxyAddress, newAddress);
+        } else {
+            proxy = InitializableImmutableAdminUpgradeabilityProxy(
+                payable(proxyAddress)
+            );
+            proxy.upgradeToAndCall(newAddress, params);
+        }
+    }
+
+    //   /**
+    //    * @notice Updates the identifier of the Aave market.
+    //    * @param newMarketId The new id of the market
+    //    */
+    //   function _setMarketId(string memory newMarketId) internal {
+    //     string memory oldMarketId = _marketId;
+    //     _marketId = newMarketId;
+    //     emit MarketIdSet(oldMarketId, newMarketId);
+    //   }
+
+    /**
+     * @notice Returns the implementation contract of the proxy contract by its identifier.
+     * @dev It returns ZERO if there is no registered address with the given id
+     * @dev It reverts if the registered address with the given id is not `InitializableImmutableAdminUpgradeabilityProxy`
+     * @param id The id
+     * @return The address of the implementation contract
+     */
+    function _getProxyImplementation(bytes32 id) internal returns (address) {
+        address proxyAddress = _addresses[id];
+        if (proxyAddress == address(0)) {
+            return address(0);
+        } else {
+            address payable payableProxyAddress = payable(proxyAddress);
+            return
+                InitializableImmutableAdminUpgradeabilityProxy(
+                    payableProxyAddress
+                ).implementation();
+        }
+    }
+}
