9a9fcbc670228337d9d861536b14be5528f0c2e0
Mihai <mihai.hanga@gmail.com>
2026-03-26 14:20:33 +0200

    updated DataTypes

--- src/libraries/types/DataTypes.sol
+++ src/libraries/types/DataTypes.sol
@@ -1,15 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.33;
 
+/**
+ * @title DataTypes
+ * @author mhng
+ * @notice Library containing data structures for the Payment Processor Protocol
+ */
+
 library DataTypes {
     /**
-     *
+     * @notice Seller platform configuration
+     * @param seller The seller's wallet address (receives stablecoin payments)
+     * @param nftContract The NFT contract address (items to be transferred)
+     * @param stablecoin The stablecoin address for payments (USDC, USDT, etc.)
+     */
+    struct SellerConfiguration {
+        address seller;
+        address nftContract;
+        address stablecoin;
+    }
+
+    /**
+     * @notice Payment information for processing
+     * @param paymentId Unique payment identifier
+     * @param buyer Buyer's wallet address
+     * @param tokenId NFT token ID to transfer
+     * @param amount Payment amount in stablecoin
      */
     struct PaymentData {
-        uint256 paymentId;
-        address seller;
+        bytes32 paymentId;
         address buyer;
-        address item;
-        uint256 price;
+        uint256 tokenId;
+        uint256 amount;
     }
 }
